7a644be1b12acc2f138b2245e615cde89d0031cf
Julian <juliamruani@gmail.com>
2024-04-14 14:42:39 -0300

    --date

--- tienda/src/App.jsx
+++ tienda/src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css"
 import Product from "./Product.jsx"
 import Marco  from "./assets/marco-aurelio.jpg"
 import Platon from "./assets/plato.jpg"
 import Julio from "./assets/julio-cesar.jpg"
 
+const URL_PRODUCTOS = "https://fakestoreapi.com/"
+
 
 function App() {
+
+   useEffect(()=>{
+    fetch
+   },[])
+
+
   return <>
-  <h2>Decoración para el hogar</h2>
+  <h2 className="h2-app">Decoración para el hogar</h2>
   <main className="producto-container">
     <Product img={Marco} descripcion="Busto de Marco Aurelio" precio="$10.000"/>
     <Product img={Platon} descripcion="Busto de Platón" precio="$10.000"/>
